30cb2b77603c236bd339e21492d4b097076f5d66
helgacozzo <helgacozzo@gmail.com>
2024-01-13 16:59:25 +0100

    Refacturing

--- app.js
+++ app.js
@@ -1,23 +1,60 @@
 import express from "express";
 import path from "path";
 import morgan from "morgan";
-const app = express();
 
+const readResource = async (resourceName) => {
+    try {
+        const data = fs.readFileSync(path.resolve(`./database/${resourceName}.json`), 'utf-8');
+        const resource = JSON.parse(data);
+        return resource;
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+const writeResource = (resourceName, resource) => {
+    const data = JSON.stringify(resource);
+    fs.writeFileSync(path.resolve(`./database/${resourceName}.json`), data);
+}
+
+const generateId = (resourceName) => {
+    const resource = readResource(resourceName);
+    const ids = books.map(b => b.id);
+    for(let i=0; i<=ids.length; i++){
+        if(!ids.includes(i)){
+            return i;
+        }
+    }
+}
+
+
+const app = express();
 app.listen(3000, () => {
-    console.log('il server è attivo e in ascolto');
+    console.log('il server è attivo e in ascolto sulla porta 3000!');
 });
 
 app.use(morgan('dev'));
-
 app.use(express.json());
 
-app.get('/autori', (req, res) => {
-    res.sendFile(path.resolve('./database/autori.json'));
+app.get('/books', (req, res) => {
+    res.sendFile(path.resolve('./database/books.json'));
 });
 
-app.post('/autori', (req, res) => {
-    const nuoviAutori = req.body;
-    res.sendFile(path.resolve('./database/autori.json'));
+app.post('/books', (req, res) => {
+    const newBook = req.body;
+    let isBookvalid = true;
+    ['title', 'author', 'year'].forEach((key) => {
+        isBookvalid &= value[key] !== undefined;
+    });
+    if(!isBookvalid){
+        res.status(400).send("Books must have title, author and year property");
+        return;
+    }
+    const books = readResource('books');
+    newBook.id = generateId('books');
+    books.push(newBook);
+    writeResource('books', books);
+    res.send(newBook);
 });
 
 
